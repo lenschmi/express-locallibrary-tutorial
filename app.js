dd078995a180cdb0343621688308087f64acadde
lenschmi <lenschmi@gmail.com>
2021-09-20 17:05:59 -0700

    Updating based on Tutorial part 5

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 
 // Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://Lena:<password>@cluster0.hdgrp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+var mongoDB = 'mongodb+srv://Lena:libraryPWORD69@cluster0.hdgrp.mongodb.net/local_library?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
